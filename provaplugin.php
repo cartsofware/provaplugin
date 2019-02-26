088dda9629525ff57551f6aa9e0e2a7266233ee3
cartsoftware <assistenza@cartsoftware.eu>
2019-02-26 12:08:31 +0100

    First commit

---
+++ provaplugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Provaplugin
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     provaplugin
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Provaplugin
+ */
+
+// Your code starts here.
